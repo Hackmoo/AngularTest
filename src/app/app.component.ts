237856a46751062842954901c403dca2d94603d7
Hackmoo <hackmo872@gmail.com>
2023-07-17 12:43:17 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-app';
+  currentVariant = 0
+  variants = [
+    'fa-solid fa-user fa-5x',
+    'fa-regular fa-user fa-5x',
+    'fa-solid fa-poo fa-5x',
+    'fa-sharp fa-solid fa-house fa-5x',
+    'fa-solid fa-cloud fa-5x',
+    'fa-solid fa-hippo fa-5x',
+    'fa-solid fa-circle-info fa-5x',
+    'fa-solid fa-circle-user fa-5x',
+    'fa-brands fa-github-square fa-5x',
+  ];
+  getRandomIcon(): void{
+    setTimeout(() => {
+      this.currentVariant = Math.round(Math.random() * this.variants.length)
+    }, 3000)
+  }
 }
